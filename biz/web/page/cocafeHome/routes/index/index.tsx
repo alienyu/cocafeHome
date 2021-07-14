aeb6be0f42574597cce58b90d29406a4d8877fde
alienyu <alienyu@foxmail.com>
2021-07-14 18:47:11 +0800

    index

--- biz/web/page/cocafeHome/routes/index/index.tsx
+++ biz/web/page/cocafeHome/routes/index/index.tsx
@@ -1,10 +1,18 @@
 import * as React from "react";
+import { withRouter } from 'react-router-dom';
 import { WrapperIndexCmp } from './styled';
 import { Row, Col } from 'antd';
 
-export default class Index extends React.Component<{}, {}> {
+const activityPic = require("@webCocafeHomeImgs/activityPic.jpg");
+
+@(withRouter as any)
+export default class Index extends React.Component<any, {}> {
     constructor(props: any) {
         super(props);
+    }
+
+    goActivityDetail = () => {
+        this.props.history.push("/activityDetail");
     }
 
     render() {
@@ -18,7 +26,78 @@ export default class Index extends React.Component<{}, {}> {
                 </Row>
                 <Row className="activityFrame" justify="center" align="middle">
                     <Row className="activityBox">
-
+                        <Row className="titleLine" justify="space-between">
+                            <Col span={6} className="title">最新活动</Col>
+                            <Col span={6} className="more">
+                                <a href="javasctipt:;" onClick={this.goActivityDetail}>更多</a>
+                            </Col>
+                        </Row>
+                        <Row className="contentBox">
+                            <Col className="eachBox">
+                                <div className="title">咖菲科技助力中国嘉德NFT加密艺术中国首拍</div>
+                                <div className="date">07.10</div>
+                                <img src={activityPic} />
+                            </Col>
+                            <Col className="eachBox">
+                                <div className="title">咖菲科技与币安中国研究院合作全球区块链数字领军课程</div>
+                                <div className="date">07.10</div>
+                                <img src={activityPic} />
+                            </Col>
+                            <Col className="eachBox">
+                                <div className="title">复旦聚劲论坛对话咖菲科技共同探讨亿万数字资产市场</div>
+                                <div className="date">07.10</div>
+                                <img src={activityPic} />
+                            </Col>
+                        </Row>
+                    </Row>
+                </Row>
+                <Row className="cooperateFrame" justify="center" align="middle">
+                    <Row className="cooperateBox">
+                        <Row className="title" justify="center">
+                            <Col span={5}>合作</Col>
+                        </Row>
+                        <Row className="contentBox">
+                            <Col span={10} className="tabFrame">
+                                <div className="tab insititution active">
+                                    <div className="subTitle">企业合作</div>
+                                    <div className="desc">
+                                        <p>助力NFT发行，焕新品牌</p>
+                                        <p>链接Z时代</p>
+                                    </div>
+                                </div>
+                                <div className="tab artist">
+                                    <div className="subTitle">艺术家合作</div>
+                                    <div className="desc">
+                                        <p>NFT艺术技术支持，精细定价方案设计</p>
+                                        <p>加密世界品牌打造</p>
+                                    </div>
+                                </div>
+                                <div className="tab ecology">
+                                    <div className="subTitle">生态伙伴合作</div>
+                                    <div className="desc">
+                                        <p>链接加密艺术世界</p>
+                                        <p>共建数字世界精神家园</p>
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col span={14} className="detailFrame">
+                                <Row>
+                                    <Col span={12}>
+                                        <div className="line"></div>
+                                        <ul className="content">
+                                            <li>助力企业发行NFT艺术品</li>
+                                            <li>获取新市场和新世代粉丝</li>
+                                            <li>焕新品牌</li>
+                                        </ul>
+                                    </Col>
+                                    <Col span={12}>dfdgdff</Col>
+                                    <Col span={12}>dfdgdff</Col>
+                                    <Col span={12}>dfdgdff</Col>
+                                    <Col span={12}>dfdgdff</Col>
+                                    <Col span={12}>dfdgdff</Col>
+                                </Row>
+                            </Col>
+                        </Row>
                     </Row>
                 </Row>
             </WrapperIndexCmp>
--- biz/web/page/cocafeHome/routes/index/styled.ts
+++ biz/web/page/cocafeHome/routes/index/styled.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 const bannerImg = require("@webCocafeHomeImgs/bg.png");
+const moreImg = require("@webCocafeHomeImgs/more.png");
+const insititutionBG = require("@webCocafeHomeImgs/insititutionBG.png");
+const artistBG = require("@webCocafeHomeImgs/artistBG.png");
+const ecologyBG = require("@webCocafeHomeImgs/ecologyBG.png");
 
 const WrapperIndexCmp = styled.div`
     .banner {
@@ -24,6 +28,125 @@ const WrapperIndexCmp = styled.div`
         background: #fff;
         .activityBox {
             width: 1040px;
+            .titleLine {
+                width: 100%;
+                height: 80px;
+                line-height: 80px;
+                border-bottom: 1px solid #333;
+                margin-bottom: 32px;
+                .title {
+                    color: #333;
+                    font-size: 40px;
+                    text-align: left;
+                }
+                .more {
+                    text-align: right;
+                    a {
+                        padding-right: 30px;
+                        color: #333;
+                        font-size: 16px;
+                        text-align: right;
+                        background: url(${moreImg}) no-repeat 100% 100%;
+                        background-position-y: -3px;
+                    }
+                }
+            }
+            .contentBox {
+                .eachBox {
+                    cursor: pointer;
+                    width: 360px;
+                    padding-left: 20px;
+                    padding-right: 20px;
+                    &:first-child {
+                        width: 340px;
+                        padding-left: 0px;
+                        border-right: 1px solid #333;
+                    }
+                    &:last-child {
+                        width: 340px;
+                        padding-right: 0px;
+                        border-left: 1px solid #333;
+                    }
+                    .title {
+                        font-size: 24px;
+                        color: #333;
+                        margin-bottom: 80px;
+                    }
+                    .date {
+                        font-size: 40px;
+                        color: #333;
+                        margin-bottom: 16px;
+                    }
+                    img {
+                        width: 320px;
+                        height: 180px;
+                    }
+                }
+            }
+        }
+    }
+    .cooperateFrame {
+        marign-top: 120px;
+        width: 100%;
+        height: 812px;
+        background: #fff;
+        .cooperateBox {
+            width: 1040px;
+            .title {
+                width: 100%; 
+                text-align: center;
+                font-size: 40px;
+                color: #333;
+                margin-bottom: 48px;
+            }
+            .contentBox {
+                width: 100%;
+                .tabFrame {
+                    .tab {
+                        width: 100%;
+                        height: 172px;
+                        cursor: pointer;
+                        padding-top: 40px;
+                        padding-left: 136px;
+                        background-color: rgba(250,250,250,0);
+                        &.insititution {
+                            background: url(${insititutionBG}) rgba(250, 250, 250, 0) no-repeat 38px 40px;
+                            &.active {
+                                background-color: rgba(250,250,250,1);
+                            }
+                        }
+                        .subTitle {
+                            font-size: 24px;
+                            color: #333;
+                            margin-bottom: 12px;
+                        }
+                        .desc {
+                            font-size: 16px;
+                            color: #666;
+                            p { margin-bottom: 0px; }
+                        }
+                    }
+                }
+                .detailFrame {
+                    background: rgba(250, 250, 250, 1);
+                    padding: 64px 44px;
+                    .line {
+                        border-top: 1px solid #333;
+                        margin-bottom: 16px;
+                    }
+                    .content {
+                        color: #000;
+                        font-size: 14px;
+                        padding-left: 15px;
+                        li {
+                            list-style: none;
+                            &:first-child { 
+                                list-style: square; 
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 `;
